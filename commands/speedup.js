c8561a16aa97f990e18ff362a87ba74321b4b5da
Snowflake <devsnowflake107@gmail.com>
2020-08-02 18:27:08 +0545

    custom filter

---
+++ commands/speedup.js
@@ -0,0 +1,21 @@
+// Custom filter (not available on discord-player)
+// check config file for example
+
+module.exports = {
+    help: {
+        name: "speedup",
+        aliases: []
+    },
+    run: async (client, message, args) => {
+        if (!message.member.voice.channel) return message.channel.send("❌ | You are not in a voice channel!");
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send("❌ | You are not in my voice channel!");
+        if (!client.player.isPlaying(message.guild.id)) {
+            return message.channel.send("❌ | I'm not playing anything?");
+        }
+        const speedup = await client.player.getQueue(message.guild.id).filters.speedup;
+        client.player.setFilters(message.guild.id, {
+            speedup: !speedup
+        });
+        return message.channel.send(`✅ | Player speed set to ${!speedup ? "1.3x" : "1x"}!`);
+    }
+};
